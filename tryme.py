b963f2b4fc51baac3a1ec959e92d721fb37e4f24
k3vinfoo <kf1270@nyu.edu>
2017-09-24 03:18:02 -0400

    hwllo

--- tryme.py
+++ tryme.py
@@ -5,12 +5,17 @@ import numpy as np
 from sklearn.cross_validation import train_test_split
 from sklearn import preprocessing
 
+tf.global_variables_initializer()
+filename = "scrapped_withRuns.csv"
+
 # X_train = np.array ([[1.,-1.,2.], [2.,0.,0.], [0.,1.,-1.]])
 # X_scaled = preprocessing.scale(X_train)
 # print(X_scaled)
 
 def get_run_value():
-    
+    df_nba = pd.read_csv(filename)
+    return df_nba['Run'] # returns 0 or 1 based on DATA -- if there is a run going on or not
+
 
 
 
@@ -22,6 +27,7 @@ def make_data(n):
 
 def fit_line(n=1, log_progress=False):
     with tf.Session() as session:
+        df_nba = pd.read_csv(filename)
         x = tf.placeholder(tf.float32, [n], name='x')
         y = tf.placeholder(tf.float32, [n], name='y')
         m = tf.Variable([1.0], trainable=True) # training variable: slope
@@ -37,8 +43,9 @@ def fit_line(n=1, log_progress=False):
         train_step = tf.train.AdamOptimizer(0.05).minimize(error)
 
         # generate input and output data with a little random noise.
-        x_in, y_star = make_data(n)
-
+        #x_in, y_star = make_data(n)
+        x_in = get_run_value()
+        y_star = df_nba['Play_Clock_Time']
         init = tf.initialize_all_variables()
         session.run(init)
         feed_dict = {x: x_in, y_act: y_star}
@@ -47,6 +54,7 @@ def fit_line(n=1, log_progress=False):
             err = np.linalg.norm(y_i - y_star, 2)
             if log_progress:
                 print("%3d | %.4f %.4f %.4e" % (i, m_i, b_i, err))
+        run_val = get_run_value()
 
         print("Done! m = %f, b = %f, err = %e, iterations = %d"
               % (m_i, b_i, err, i))
@@ -55,6 +63,7 @@ def fit_line(n=1, log_progress=False):
         print(" Actual: %s" % y_star)
 
 fit_line()
+print("done")
 
 
 
