b105b1eb3c6c4eaea39ebdb11bbfe8d160c68ad4
k3vinfoo <kf1270@nyu.edu>
2017-09-24 02:23:40 -0400

    got slope working

--- tryme.py
+++ tryme.py
@@ -12,22 +12,22 @@ from sklearn import preprocessing
 def make_data(n):
     np.random.seed(42) # To ensure same data for multiple runs
     x = 2.0 * np.array(range(n))
-    y = 1.0 + 3.0 * (np.array(range(n)) + 0.1 * (np.random.rand(n) – 0.5))
+    y = 1.0 + 3.0 * (np.array(range(n)) + 0.1 * (np.random.rand(n) - 0.5))
     return x, y
 
 def fit_line(n=1, log_progress=False):
     with tf.Session() as session:
-        x = tf.placeholder(tf.float32, [n], name=’x’)
-        y = tf.placeholder(tf.float32, [n], name=’y’)
+        x = tf.placeholder(tf.float32, [n], name='x')
+        y = tf.placeholder(tf.float32, [n], name='y')
         m = tf.Variable([1.0], trainable=True) # training variable: slope
         b = tf.Variable([1.0], trainable=True) # training variable: intercept
-        y = tf.add(tf.mul(m, x), b) # fit y_i = m * x_i + b
+        y = tf.add(tf.multiply(m, x), b) # fit y_i = m * x_i + b
 
         # actual values (for training)
-        y_act = tf.placeholder(tf.float32, [n], name=’y_’)
+        y_act = tf.placeholder(tf.float32, [n], name='y_')
 
         # minimize sum of squared error between trained and actual.
-        error = tf.sqrt((y – y_act) * (y – y_act))
+        error = tf.sqrt((y - y_act) * (y - y_act))
         # train_step = tf.train.GradientDescentOptimizer(0.01).minimize(error)
         train_step = tf.train.AdamOptimizer(0.05).minimize(error)
 
@@ -39,18 +39,17 @@ def fit_line(n=1, log_progress=False):
         feed_dict = {x: x_in, y_act: y_star}
         for i in range(30 * n):
             y_i, m_i, b_i, _ = session.run([y, m, b, train_step], feed_dict)
-            err = np.linalg.norm(y_i – y_star, 2)
+            err = np.linalg.norm(y_i - y_star, 2)
             if log_progress:
-                print(“%3d | %.4f %.4f %.4e” % (i, m_i, b_i, err))
+                print("%3d | %.4f %.4f %.4e" % (i, m_i, b_i, err))
 
-        print(“Done! m = %f, b = %f, err = %e, iterations = %d”
+        print("Done! m = %f, b = %f, err = %e, iterations = %d"
               % (m_i, b_i, err, i))
-        print(”      x: %s” % x_in)
-        print(“Trained: %s” % y_i)
-        print(” Actual: %s” % y_star)
+        print("      x: %s" % x_in)
+        print("Trained: %s" % y_i)
+        print(" Actual: %s" % y_star)
+
+fit_line()
 
 
 
-
-
-        
